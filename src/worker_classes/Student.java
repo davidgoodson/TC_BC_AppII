6f7151a371e94d2611f3731f537bd6351596fffe
Buyinza David <davidbuyinza256@gmail.com>
2021-03-20 11:38:55 +0300

    ft(add: working with objects in ArrayLists)

--- src/worker_classes/Student.java
+++ src/worker_classes/Student.java
@@ -5,10 +5,58 @@
  */
 package worker_classes;
 
+import java.util.Date;
+
 /**
  *
  * @author david
  */
 public class Student {
+    private String name;
+    private String address;
+    private Date dob;
+    private String gender;
+    
+    public Student(){}
+
+    public Student(String name, String address, Date dob, String gender) {
+        this.name = name;
+        this.address = address;
+        this.dob = dob;
+        this.gender = gender;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
     
 }
